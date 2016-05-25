d1a516d6cffe3a5468d9467aad2c193b9062d329
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2016-05-25 21:57:53 +0100

    Adding DataProcessor indicator

--- pythonbacktest/indicator/__init__.py
+++ pythonbacktest/indicator/__init__.py
@@ -6,5 +6,6 @@ from .abstractindicator import AbstractIndicator
 from .indicators import Indicators
 from .sma import SMA
 from .differential import Differential
+from .dataprocessor import DataProcessor
 from .indicatorhistory import IndicatorHistory
 from .indicatorsnapshot import IndicatorsSnapshot
---
+++ pythonbacktest/indicator/dataprocessor.py
@@ -0,0 +1,41 @@
+from .abstractindicator import AbstractIndicator
+
+
+class DataProcessor(AbstractIndicator):
+
+    def __init__(self, processing_proc):
+        self.__current_value = None
+        self.__all_values = []
+        self.__all_results = []
+
+        if processing_proc is None:
+            raise ValueError("processing_proc must be set")
+
+        self.__processing_proc = processing_proc
+
+    @property
+    def result(self):
+        return self.__all_results[-1]
+
+    @property
+    def all_result(self):
+        return self.__all_results
+
+    def on_new_upstream_value(self, new_value):
+
+        if new_value is None:
+            self.__all_values.append(None)
+        else:
+            if type(new_value) is list:
+                del self.__all_values[:(len(new_value) - 1)]
+                self.__all_values.extend(new_value)
+            else:
+                self.__all_values.append(new_value)
+
+            # ... and recalculate entire data on processing function
+            self.__process_all_values()
+
+    def __process_all_values(self):
+        for value in self.__all_values:
+            single_result = self._processing_proc(value)
+            self.__all_results.append(single_result)

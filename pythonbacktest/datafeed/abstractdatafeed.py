30153b70a209f381b7175353cbf55bd91a816b65
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2016-10-28 18:40:57 +0100

    Adding view range to the datafeed

--- pythonbacktest/datafeed/abstractdatafeed.py
+++ pythonbacktest/datafeed/abstractdatafeed.py
@@ -7,11 +7,20 @@ class AbstractDataFeed(object):
         # value - collection of TradingDayData ordered by date for the given day
         self.__data = {}
 
+        self.__data_view_range = (0, None)
+
     @property
     def all_data(self):
         return self.__data
 
+    def set_data_view_range(self, min_index = 0, max_index = None):
+        self.__data_view_range = (min_index, max_index)
+
     # get data for single day only
     # return price bars for the given date
     def get_prices_bars_for_day(self, trading_day):
-        return self.__data[trading_day].price_bars
+
+        if self.__data_view_range[1] is None:
+            return self.__data[trading_day].price_bars[self.__data_view_range[0]:]
+        else:
+            return self.__data[trading_day].price_bars[self.__data_view_range[0]:self.__data_view_range[1] + 1]

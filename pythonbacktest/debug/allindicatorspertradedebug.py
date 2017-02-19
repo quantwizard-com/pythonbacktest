4ed33c98a592266ae3061356081108d58e7f8b5a
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2017-02-19 15:14:52 +0000

    Adding line formatting into single place

--- pythonbacktest/debug/allindicatorspertradedebug.py
+++ pythonbacktest/debug/allindicatorspertradedebug.py
@@ -1,5 +1,4 @@
 class AllIndicatorsPerTradeDebug(object):
-
     def render(self, trade_log, *indicators_to_render):
 
         all_transactions = trade_log.all_transactions
@@ -12,32 +11,48 @@ class AllIndicatorsPerTradeDebug(object):
         print (self.__render_header(all_transactions, indicators_to_render))
 
         for transaction in all_transactions:
-            single_transaction_string = "%s\t%s\t%s" % (transaction.price_bar_index_per_day, str(transaction.timestamp), transaction.transaction_type)
+            single_transaction_string = ""
+            single_transaction_string = self.__append_string_with_tab(single_transaction_string,
+                                                                      transaction.price_bar_index_per_day,
+                                                                      transaction.timestamp,
+                                                                      transaction.transaction_type)
 
             indicators = transaction.all_indicators_values
 
             if (indicators_to_render):
                 for indicator_name in indicators_to_render:
-                    single_transaction_string += "\t" + str(indicators[indicator_name])
+                    single_transaction_string = self.__append_string_with_tab(single_transaction_string,
+                                                                              indicators[indicator_name])
             else:
                 for key, value in indicators.iteritems():
-                    single_transaction_string += "\t" + str(value)
+                    single_transaction_string = self.__append_string_with_tab(single_transaction_string, value)
 
             print single_transaction_string
 
     def __render_header(self, all_transactions, indicators_to_render):
-
         # set static header fields
-        header_string = "Index\tTimestamp\tType"
+        header_string = ""
+        header_string = self.__append_string_with_tab(header_string, "Index", "Timestamp", "Type")
 
         if indicators_to_render:
             for indicator_name in indicators_to_render:
-                header_string += "\t" + indicator_name
+                header_string = self.__append_string_with_tab(header_string, indicator_name)
         else:
             # get first transaction to render the header
             indicators = all_transactions[0].all_indicators_values
 
             for key, value in indicators.iteritems():
-                header_string += "\t" + key
+                header_string = self.__append_string_with_tab(header_string, key)
 
         return header_string
+
+    def __append_string_with_tab(self, target_string, *values_to_append):
+
+        updated_target_string = target_string
+        for value_to_append in values_to_append:
+            if len(updated_target_string) > 0:
+                updated_target_string += "\t"
+
+            updated_target_string += str(value_to_append)
+
+        return updated_target_string

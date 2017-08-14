e37276e46f8d1f22733e7e636dcb5e34e75a7269
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2017-08-14 12:52:35 +0100

    Adding trend tracking function

--- pythonbacktest/indicator/trendtracker.py
+++ pythonbacktest/indicator/trendtracker.py
@@ -10,7 +10,13 @@ class TrendTracker(AbstractIndicator):
         self.__all_input_data = []
 
     def on_new_upstream_value(self, new_value):
-        self.__all_input_data.append(new_value)
+        if type(new_value) is list:
+            del self.__all_input_data[:len(new_value)]
+            self.__all_input_data.extend(new_value)
+        else:
+            self.__all_input_data.append(new_value)
+
+        self.__recalculate_all_result()
 
     @property
     def result(self):
@@ -18,32 +24,54 @@ class TrendTracker(AbstractIndicator):
 
     @property
     def all_result(self):
-        pass
+        return self.__all_results
 
-    def __recalculate_result(self, looking_for_support):
-        self.reset()
+    def __recalculate_all_result(self):
+        self.__all_results = self.__recalculate_single_serie_result()
 
-        looking_for_resistance = not looking_for_support
-        baseline = -1.0
+    def __recalculate_single_serie_result(self):
+
+        previous_numerical_record = None
+        previous_numerical_record_index = None
+        local_minimum = None
+        local_maximum = None
+        looking_for_minimum = None
+        looking_for_maximum = None
+        first_record = True
         current_index = 0
-        reference_index = 0
-        reference_angle = 999999999.99 if looking_for_support else -999999999.99
-        results = []
 
-        for input_number in self.__all_input_data:
-            if input_number is not None:
+        all_results = []
 
-                if baseline == -1.0:
-                    baseline = input_number
-                else:
-                    current_angle = (input_number - baseline) / current_index
+        for single_record in self.__all_input_data:
+            if single_record is not None:
+                if previous_numerical_record is not None:
+                    if single_record > previous_numerical_record:
+                        if looking_for_minimum:
+                            #we've hit a local minimum
+                            all_results[previous_numerical_record_index] = (previous_numerical_record, None)
 
-                    if looking_for_support and current_angle <= reference_angle:
-                        reference_angle = current_angle
-                    elif looking_for_resistance and current_angle >= reference_angle:
-                        reference_angle = current_angle
+                            looking_for_maximum = True
+                            looking_for_minimum = False
 
-            else:
-                self.__all_results.append(None)
+                        if looking_for_maximum is None:
+                            looking_for_maximum = True
+
+                    if single_record < previous_numerical_record:
+                        if looking_for_maximum:
+                            #we've hit a local maximum
+                            all_results[previous_numerical_record_index] = (None, previous_numerical_record)
+
+                            looking_for_maximum = False
+                            looking_for_minimum = True
+
+                        if looking_for_minimum is None:
+                            looking_for_minimum = True
+
+                previous_numerical_record = single_record
+                previous_numerical_record_index = current_index
+
+            all_results.append((None, None))
 
             current_index += 1
+
+        return all_results
--- testcases/indicator_tests/trendtrackertests.py
+++ testcases/indicator_tests/trendtrackertests.py
@@ -17,3 +17,69 @@ class TrendTrackerTests(unittest.TestCase):
 
         self.assertEqual(expected_result, actual_result)
 
+    def test_single_support_point(self):
+        test_data = [5, 4, 3, 4, 5]
+        expected_result = [(None, None), (None, None), (3, None), (None, None), (None, None)]
+
+        trend_tracker = TrendTracker()
+
+        trend_tracker.on_new_upstream_value(test_data)
+
+        actual_result = trend_tracker.all_result
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_single_support_point_mix_with_nones(self):
+        test_data = [None, 5, 4, None, None, 3, None, 4, 5, None]
+        expected_result = \
+            [(None, None), (None, None),
+             (None, None), (None, None),
+             (None, None), (3, None),
+             (None, None), (None, None),
+             (None, None), (None, None)]
+
+        trend_tracker = TrendTracker()
+
+        trend_tracker.on_new_upstream_value(test_data)
+
+        actual_result = trend_tracker.all_result
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_single_resistance_point(self):
+        test_data = [3, 4, 5, 4, 3]
+        expected_result = [(None, None), (None, None), (None, 5), (None, None), (None, None)]
+
+        trend_tracker = TrendTracker()
+        trend_tracker.on_new_upstream_value(test_data)
+
+        actual_result = trend_tracker.all_result
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_resistance_then_support(self):
+        test_data = [3, 4, 5, 4, 3, 2, 1, 2, 3, 4]
+        expected_result = [
+            (None, None), (None, None), (None, 5),
+            (None, None), (None, None), (None, None),
+            (1, None), (None, None), (None, None), (None, None)]
+
+        trend_tracker = TrendTracker()
+        trend_tracker.on_new_upstream_value(test_data)
+
+        actual_result = trend_tracker.all_result
+
+        self.assertEqual(expected_result, actual_result)
+
+    def test_rapid_changes(self):
+        test_data = [3, 4, 3, 4, 3, 4, 3]
+        expected_result = [
+            (None, None), (None, 4), (3, None), (None, 4), (3, None), (None, 4), (None, None)
+        ]
+
+        trend_tracker = TrendTracker()
+        trend_tracker.on_new_upstream_value(test_data)
+
+        actual_result = trend_tracker.all_result
+
+        self.assertEqual(expected_result, actual_result)

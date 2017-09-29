a235a61773b4a00995d64611b7de770052047358
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2017-09-29 15:15:28 +0100

    Simplifying the IndicatorHistory object

--- pythonbacktest/indicatorcalculator/indicatorhistory.py
+++ pythonbacktest/indicatorcalculator/indicatorhistory.py
@@ -11,46 +11,32 @@ class IndicatorHistory(object):
 
     def __init__(self):
 
-        # dictonary of dictionaries storing snapshots of the indicators
-        # - key - date of the price bars and indicators
-        # - value - dictionary: indicators for all data points during that day
+        # dictionary of dictionaries storing snapshots of the indicators
         # -- key - date and time of indicators
         # -- value - IndicatorsSnapshot object with all collected indicators
         self.__indicators_storage = OrderedDict()
 
     def store_snapshot(self, timestamp, indicators_snapshot):
-        date = timestamp.date()
 
-        if date not in self.__indicators_storage:
-            self.__indicators_storage[date] = OrderedDict()
-
-        if timestamp in self.__indicators_storage[date]:
+        if timestamp in self.__indicators_storage:
             raise ValueError("Duplicated indicators for timestamp: " + str(timestamp))
 
         # ok, time to copy indicators into a snapshot
-        self.__indicators_storage[date][timestamp] = indicators_snapshot
+        self.__indicators_storage[timestamp] = indicators_snapshot
 
     # return sorted list of tuples (sorted by datetime)
     # value 1: date time of when the indicator has been recorded
     # value 2: actual snapshot of the Indicators object
     # e.g.: [(2016-01-02 14:30.00, IndicatorsSnapshot),(2016-01-02 14:30.05, IndicatorsSnapshot)]
-    def get_indicator_history_for_day(self, day_date):
-        data_per_day = self.__indicators_storage[day_date]
+    def get_indicators_history(self):
+        data_per_day = self.__indicators_storage
 
         # return list of key, values, where: key is datetime and value: all collected indicators for that timestamp
         return data_per_day.items()
 
     # get last snapshot of all recorded indicators for the given day
-    def get_last_indicator_snapshot_per_day(self, day_date):
-        data_per_day = self.__indicators_storage[day_date]
+    def get_last_indicators_snapshot(self):
+        data_per_day = self.__indicators_storage
         last_key = list(data_per_day.keys())[-1]
 
         return data_per_day[last_key]
-
-    @property
-    def all_indicator_names(self):
-        """
-        Return names of all stored indicators
-        """
-        return self.__indicator_names
-

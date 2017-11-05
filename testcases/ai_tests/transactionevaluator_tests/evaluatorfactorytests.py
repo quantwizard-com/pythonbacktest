c957a9e419a4ca0eab18b4c2d8ffdeb746cbc67f
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2017-11-05 22:38:04 +0000

    Testing negative nodes

--- testcases/ai_tests/transactionevaluator_tests/evaluatorfactorytests.py
+++ testcases/ai_tests/transactionevaluator_tests/evaluatorfactorytests.py
@@ -5,7 +5,7 @@ from pythonbacktest.ai.transactionevaluator.evaluatorfactory import EvaluatorFac
 
 class EvaluatorFactoryTests(unittest.TestCase):
 
-    def test_create_evaluator_function_body_single_node_name(self):
+    def test_create_evaluator_single_node_name(self):
 
         evaluator_map = {
             "buy": ["sn_buy_1"],
@@ -22,3 +22,38 @@ class EvaluatorFactoryTests(unittest.TestCase):
 
         self.assertEqual(expected_body, function_body)
 
+    def test_create_evaluator_single_negative_node_name(self):
+
+        evaluator_map = {
+            "buy": ["!sn_buy_1"],
+            "sell": None,
+            "ssell": None
+        }
+        nodes_dict_name = "nodes1234"
+
+        evaluator_factory = EvaluatorFactory()
+        function_body = evaluator_factory._EvaluatorFactory__create_evaluator_function_body(evaluator_map, nodes_dict_name)
+        expected_body = f"buy=not {nodes_dict_name}['sn_buy_1'].current_node_result\nsell=False\nssell=False"
+
+        self.assertEqual(expected_body, function_body)
+
+
+    def test_create_evaluator_negative_sell_node(self):
+
+        evaluator_map = {
+            "buy": ["sn_buy_1", "sn_buy_2"],
+            "sell": ["sn_sell_1", "!sn_sell_2"],
+            "ssell": None
+        }
+        nodes_dict_name = "nodes1234"
+
+        evaluator_factory = EvaluatorFactory()
+
+        function_body = evaluator_factory._EvaluatorFactory__create_evaluator_function_body(evaluator_map, nodes_dict_name)
+
+        expected_body = f"buy={nodes_dict_name}['sn_buy_1'].current_node_result and " \
+                        f"{nodes_dict_name}['sn_buy_2'].current_node_result\n" \
+                        f"sell={nodes_dict_name}['sn_sell_1'].current_node_result and " \
+                        f"not {nodes_dict_name}['sn_sell_2'].current_node_result\nssell=False"
+
+        self.assertEqual(expected_body, function_body)

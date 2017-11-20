9ea00a8508ff423d5a947ab652571c80051c48b6
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2017-11-20 22:09:08 +0000

    Adding multiday performance calculator

--- pythonbacktest/ai/indicatorshistoryprocessor/multiday/multidayhistoryprocessor.py
+++ pythonbacktest/ai/indicatorshistoryprocessor/multiday/multidayhistoryprocessor.py
@@ -1,5 +1,57 @@
+from collections import OrderedDict
+from typing import Dict
+
+from pythonbacktest.ai.backoffice.backtestbackoffice.backofficefactory import BackOfficeFactory
+from pythonbacktest.ai.indicatorshistoryprocessor.singleday import BacktestHistoryProcessorFactory
+from pythonbacktest.ai.strategyperformance.calculators.abstractperfcalculator import AbstractPerfCalculator
+from pythonbacktest.indicatorcalculator import IndicatorsMap, IndicatorsCalculator
+from pythonbacktest.indicatorshistory import IndicatorHistory, ReferencialSnapshot
+
+
 class MultidayHistoryProcessor(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, multiday_data: OrderedDict, indicators_map_definition: Dict,
+                 nodes_map_definition, evaluator_map, performance_calculator: AbstractPerfCalculator,
+                 initial_budget, default_transaction_size):
+
+        self.__multiday_data = multiday_data
+        self.__indicators_map_definition = indicators_map_definition
+        self.__nodes_map_definition = nodes_map_definition
+        self.__evaluator_map = evaluator_map
+        self.__performance_calculator = performance_calculator
+        self.__initial_budget = initial_budget
+        self.__default_transaction_size = default_transaction_size
+
+    def run_processor(self):
+        multiday_performance_report = OrderedDict()
+
+        for date, price_bars in self.__multiday_data.items():
+
+            # calculate indicators history
+            indicators_history = self.__calculate_history(indicators_history)
+
+            # get the performance report for the single day
+            multiday_performance_report[date] = self.__run_back_office_get_performance_report(indicators_history)
+
+    def __calculate_history(self, price_bars):
+        indicators_map = IndicatorsMap(indicators_map_definition=self.__indicators_map_definition)
+        indicators_history = IndicatorHistory(ReferencialSnapshot)
+        indicators_calculator = IndicatorsCalculator(indicators_map, target_indicators_history=indicators_history)
+        return indicators_calculator.run_calculation(price_bars=price_bars)
+
+    def __run_back_office_get_performance_report(self, indicators_history):
+        back_test_back_office = BackOfficeFactory.create_back_test_back_office(
+            initial_budget=self.__initial_budget, default_transaction_size=self.__default_transaction_size)
+
+        history_processor = BacktestHistoryProcessorFactory.create_processor_factory(
+            indicators_history=indicators_history,
+            nodes_map_definition=self.__nodes_map_definition,
+            evaluator_map=self.__evaluator_map,
+            back_test_back_office=back_test_back_office,
+            performance_calculator=self.__performance_calculator)
+
+        return history_processor.run_processor()
+
+
+
 
